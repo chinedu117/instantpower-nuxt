f71831048f111ac0331a042acd7177c997d9cd58
chinedu <chinedudimonyeka@gmail.com>
2019-05-29 13:32:15 +0100

    Skeleton

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,31 @@
 <template>
-  <v-app dark>
+  <v-app dark="false"\
+    v-scroll="scrolled = true"
+  >
+
+
     <v-navigation-drawer
+      width="100%"
+      class="overlay"
       v-model="drawer"
-      :mini-variant="miniVariant"
       :clipped="clipped"
       fixed
       app
+      v-if="$vuetify.breakpoint.smAndDown"
     >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+      <ul>
+          <li v-for="item in items">
+              {{ item.title }}
+          </li>
+      </ul>
     </v-navigation-drawer>
+
+
     <v-toolbar
-      :clipped-left="clipped"
       fixed
       app
+      :class=" scrolled ? 'camouflage' : '' "
+      flat="!scrolled"
     >
       <v-toolbar-side-icon @click="drawer = !drawer" />
       <v-btn
@@ -58,9 +56,9 @@
       </v-btn>
     </v-toolbar>
     <v-content>
-      <v-container>
+    
         <nuxt />
-      </v-container>
+
     </v-content>
     <v-navigation-drawer
       v-model="rightDrawer"
@@ -90,6 +88,7 @@
 export default {
   data() {
     return {
+      scrolled: false,
       clipped: false,
       drawer: false,
       fixed: false,
@@ -113,3 +112,14 @@ export default {
   }
 }
 </script>
+<style>
+.camouflage {
+
+    background-color: rgba(186,196,209,.31373) !important;
+}
+
+.overlay {
+    opacity: 0.5;
+    background-color: black;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,100 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>For more information on Vuetify, check out the <a
-            href="https://vuetifyjs.com"
-            target="_blank"
-          >documentation</a>.</p>
-          <p>If you have questions, please join the official <a
-            href="https://chat.vuetifyjs.com/"
-            target="_blank"
-            title="chat"
-          >discord</a>.</p>
-          <p>Find a bug? Report it on the github <a
-            href="https://github.com/vuetifyjs/vuetify/issues"
-            target="_blank"
-            title="contribute"
-          >issue board</a>.</p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >Nuxt Documentation</a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >Nuxt GitHub</a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/inspire"
-          >Continue</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+<div>
+ <section class="landing-page-background">
+    <v-container class="justify-content-start">
+
+     <div class="image-box">
+        
+     </div>
+     <v-layout class="mt-2">
+        <v-flex xs12 md6>
+            <h1 class="display-3 font-weight-bold">Reliable, Convenient, Electricity for all.</h1>   
+        </v-flex>
+     </v-layout>
+
+
+
+     <v-layout row fluid>
+      <v-flex xs12 md8>
+
+        <v-card class="elevate-2 card_custom">
+
+          <v-container>
+
+            
+          
+            <v-layout wrap class="my-2"><v-flex xs12><h2 class="display-1 "> Buy Electricity in 60 Seconds </h2> </v-flex></v-layout>
+          
+          
+            <v-layout>
+              <v-flex xs12>
+                <v-text-field outline placeholder="Your Meter Number here"></v-text-field> 
+               </v-flex>
+            </v-layout>
+          
+          
+            <v-layout>
+              <v-flex xs12>
+               <v-text-field outline placeholder="Amount in Naira"></v-text-field> 
+            </v-flex>
+            </v-layout>
+          
+          
+            <v-layout>
+              <v-flex xs12>
+                <v-text-field outline placeholder="Your Phone Number"></v-text-field>
+            </v-flex>
+            </v-layout>
+          
+          
+            <v-layout>
+              <v-flex xs12>
+                <v-text-field outline placeholder="Your Meter Number here"></v-text-field> 
+            </v-flex>
+            </v-layout>
+          
+          
+            <v-layout>
+              <v-flex xs12>
+                <v-text-field outline placeholder="Email Address"></v-text-field>
+            </v-flex>
+            </v-layout>
+          
+          
+            <v-layout>
+              <v-flex xs12>
+                   <v-btn width="100%"  outline class="mx-0 my-3">Buy Electricity Now<v-icon>user</v-icon></v-btn>
+               </v-flex>
+            </v-layout>
+          
+           </v-container>
+        </v-card>
+     </v-flex>
+   </v-layout>
+   </v-container>
+</section>
+
+
+   <section class="second-band"> 
+     <v-container> 
+       <v-layout  wrap class="justify-end" >
+          <v-flex xs12 md4 >
+            
+              <h2 class="display-3  font-weight-bold pt-2">The most convenient way to buy power</h2>
+
+          </v-flex>
+       </v-layout>
+      </v-container>
+
+   </section>
+   
+
+ 
+  
+  
+</div>
+
+ 
 </template>
 
 <script>
@@ -71,3 +108,51 @@ export default {
   }
 }
 </script>
+<style>
+.card_custom {
+   border-radius: 7px;
+
+}
+
+
+ .image-box {
+   display: hidden;
+
+}
+
+@media screen and (min-width: 900px){
+   .card_custom {
+      width: 600px;
+
+  }
+
+  .image-box{
+   display: block;
+   padding: 0;
+   width: 300px;
+   height: 300px;
+   position: absolute;
+   left: 50%;
+   bottom: 0px;
+   background-color: grey;
+
+}
+
+}
+
+.landing-page-background {
+   background-color: rgba(186,196,209,.31373) !important;
+   z-index: 2;
+   height: 70vh;
+   position: relative;
+}
+
+
+
+.second-band {
+   background-color: #fff;
+   min-height: 60vh;
+}
+
+
+</style>
